9384727f964664e47425023109db9d9b0127f25c
Bruno Lourenco <brunnolou@gmail.com>
2019-04-26 16:55:01 +0300

    Add types for useMorphKeys

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as useMorph } from './useMorph';
 export { default as useMorphs } from './useMorphs';
-export { default as useMorphList } from './useMorphs';
-export { default as useMorphKeys } from './useMorphKeys';
+export { default as useMorphKeys } from './useMorphs';
+export { default as useMorphList } from './useMorphList';
 export { default as useFade } from './useFade';
 export { default as presets } from './presets';
--- src/useMorphs.ts
+++ src/useMorphs.ts
@@ -1,23 +1,86 @@
 import useMorph from './useMorph';
-import { MorphOptions } from './types';
+import { MorphOptions, Morph } from './types';
 
-type OptionsMapperFn = (items: any[]) => MorphOptions;
-
-const useMorphs = (
-  items: any[] | number,
-  options: OptionsMapperFn | MorphOptions = {},
-) => {
-  let list = items;
-
-  if (typeof list === 'number') list = Array(list).fill(0);
-
-  const optionsFn = typeof options === 'function' ? options : () => options;
-
-  return list
-    .map(optionsFn)
-    .map((options: MorphOptions, i: number) =>
-      useMorph({ zIndex: i, ...options }),
-    );
+type ObjectKey = {
+  key?: string;
 };
+
+function useMorphs<
+  A extends string,
+  B extends string,
+  C extends string,
+  D extends string,
+  E extends string,
+  F extends string,
+  G extends string,
+  H extends string
+>(
+  keys: [A, B, C, D, E, F, G, H],
+  options?: MorphOptions,
+): { [key in A | B | C | D | E | F | G | H]: Morph };
+
+function useMorphs<
+  A extends string,
+  B extends string,
+  C extends string,
+  D extends string,
+  E extends string,
+  F extends string,
+  G extends string
+>(
+  keys: [A, B, C, D, E, F, G],
+  options?: MorphOptions,
+): { [key in A | B | C | D | E | F | G]: Morph };
+
+function useMorphs<
+  A extends string,
+  B extends string,
+  C extends string,
+  D extends string,
+  E extends string,
+  F extends string
+>(
+  keys: [A, B, C, D, E, F],
+  options?: MorphOptions,
+): { [key in A | B | C | D | E | F]: Morph };
+
+function useMorphs<
+  A extends string,
+  B extends string,
+  C extends string,
+  D extends string,
+  E extends string
+>(
+  keys: [A, B, C, D, E],
+  options?: MorphOptions,
+): { [key in A | B | C | D | E]: Morph };
+
+function useMorphs<
+  A extends string,
+  B extends string,
+  C extends string,
+  D extends string
+>(
+  keys: [A, B, C, D],
+  options?: MorphOptions,
+): { [key in A | B | C | D]: Morph };
+
+function useMorphs<A extends string, B extends string, C extends string>(
+  keys: [A, B, C],
+  options?: MorphOptions,
+): { [key in A | B | C]: Morph };
+
+function useMorphs<A extends string, B extends string>(
+  keys: [A, B],
+  options?: MorphOptions,
+): { [key in A | B]: Morph };
+
+function useMorphs(keys, options = {}): ObjectKey {
+  const morphObject = keys.reduce((acc: Morph, key: string, i: number) => {
+    return { ...acc, [key]: useMorph({ zIndex: i, ...options }) };
+  }, {});
+
+  return morphObject;
+}
 
 export default useMorphs;
